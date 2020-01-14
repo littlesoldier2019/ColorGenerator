04e1de79b9e4170f288b22d1ff380de92d353094
littlesoldier2019 <hongngoc.ftu2011@gmail.com>
2020-01-14 16:20:15 +0200

    form validation

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+
     const randomColor = function () {
         let letter = '0123456789ABCDEF';
         let colorCode = "#";
@@ -7,12 +8,48 @@ window.onload = function() {
             colorCode += letter[Math.floor(Math.random()*16)]
         } return colorCode;
     } 
+     
     
-    console.log(randomColor())
+    document.querySelector("div.input").style.backgroundColor = randomColor();
+    
+    let colorDisplay = document.querySelector(".color__display")
+    
+    const colorBlock = function (num) {
+        for (let i = 0; i < num; i++) {
+            let block = document.createElement("div");
+            block.classList.add("color__display-item");
+            block.style.backgroundColor = randomColor();
+            block.textContent = randomColor(); 
+            colorDisplay.append(block);
+        }
+    }
+
+    let clearForm = function() {
+        let inputNum = document.getElementById("input__number");
+        inputNum.value = "";
+    }
  
-    // document.getElementById("top__color").style.backgroundColor = randomColor()
-    document.querySelector("div.top").style.backgroundColor = randomColor();
-    document.querySelector("color__display").style.backgroundColor = randomColor();
+
+    let submit = document.getElementById("generate");
+   
+        submit.onclick = function() {  
+        let inputNum = document.getElementById("input__number");
+        let num = inputNum.value;
+
+        if (isNaN(num) || num < 5 || num > 100) {
+            alert("Enter a valid number from 5 to 100")
+        } else {
+            colorBlock(num);
+        } 
+        
+        clearForm();
+    }
     
+    
+    let form = document.querySelector(".user__input");
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+    });
+
 }
 
