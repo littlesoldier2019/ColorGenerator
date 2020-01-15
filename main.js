1d3b37b395a4fc1e2ca23f4386f58ddad18824df
littlesoldier2019 <hongngoc.ftu2011@gmail.com>
2020-01-15 22:52:38 +0200

    set timer

--- main.js
+++ main.js
@@ -1,55 +1,105 @@
-window.onload = function() {
+window.addEventListener("load",colorGenerator); 
+
+function colorGenerator() {
 
     const randomColor = function () {
-        let letter = '0123456789ABCDEF';
+        let letter = '0123456789abcdef';
         let colorCode = "#";
     
         for (let i = 0; i < 6; i++) {
             colorCode += letter[Math.floor(Math.random()*16)]
         } return colorCode;
     } 
-     
+    
+    
     
     document.querySelector("div.input").style.backgroundColor = randomColor();
     
-    let colorDisplay = document.querySelector(".color__display")
+    let colorDisplay = document.querySelector(".color__display");
+    let setTimer;
     
-    const colorBlock = function (num) {
+    function colorBlock(num) {
+        
+        let blockArr = [];
+
         for (let i = 0; i < num; i++) {
+            
+            let color = randomColor();
+
             let block = document.createElement("div");
             block.classList.add("color__display-item");
-            block.style.backgroundColor = randomColor();
-            block.textContent = randomColor(); 
+            block.style.backgroundColor = color;
             colorDisplay.append(block);
+
+            let colorText = document.createElement("p");
+            colorText.classList.add("color__display-text");
+            colorText.innerText = color;
+            block.append(colorText);
+
+            let button = document.createElement("button");
+            button.classList.add("color__display-copy");
+            button.innerText = "Copy";
+            button.value = color;
+            block.append(button);
+
+            button.onclick = function() {
+                let textArea = document.createElement("textarea");
+                textArea.value = button.value;
+                document.body.appendChild(textArea);
+                textArea.select();
+                document.execCommand("Copy");
+                textArea.remove();
+            }
+
+            blockArr.push(block);
+
+        }
+
+        setTimer = setInterval(function () {
+            for (let i = 0; i < num; i++) {
+                let colorChange = randomColor()
+                blockArr[i].style.backgroundColor = colorChange;
+                blockArr[i].querySelector("p").innerText = colorChange; 
+                blockArr[i].querySelector("button").value = colorChange;
+            }     
+        }, 2000)
+    }
+  
+    
+    function clearForm() {
+        if (colorDisplay.childElementCount != 0) {
+            colorDisplay.innerHTML = "";
         }
     }
 
-    let clearForm = function() {
-        let inputNum = document.getElementById("input__number");
-        inputNum.value = "";
-    }
- 
-
+    
     let submit = document.getElementById("generate");
-   
-        submit.onclick = function() {  
+    submit.addEventListener("click", generateColor);
+
+    function generateColor() {  
         let inputNum = document.getElementById("input__number");
         let num = inputNum.value;
+
 
         if (isNaN(num) || num < 5 || num > 100) {
             alert("Enter a valid number from 5 to 100")
         } else {
+            clearForm();
             colorBlock(num);
-        } 
-        
-        clearForm();
+        }    
     }
-    
-    
+
+    let stop = document.getElementById("stop");
+    stop.addEventListener("click", stopTimer);
+
+    function stopTimer() {
+        clearInterval(setTimer);
+    }
+
     let form = document.querySelector(".user__input");
     form.addEventListener('submit', (e) => {
         e.preventDefault();
     });
-
+    
 }
 
